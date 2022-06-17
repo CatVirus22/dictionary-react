fd7a977475bbf4e0af2612d69a8d6d5d5d656802
Marizabelle Aldana <94816917+CatVirus22@users.noreply.github.com>
2022-06-16 21:59:33 -0600

    Set up Dictionary proyect

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App container pt-5">
+          <div className="row d-flex justify-content-start">
+            <p>Dictionary app UwU</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Mirri Cuca Leyen
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
